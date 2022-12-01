95b0ae406650f8120cbdd6a708cdadcb3bb92db9
Sbbhosale <sb.bhosaleshubham@gmail.com>
2022-12-02 00:08:53 +0530

    feat : completed feature of sound on click

---
+++ app.js
@@ -0,0 +1,7 @@
+window.addEventListener('keydown', function(e) {
+
+    let audio = document.querySelector(`audio[data-key="${e.keyCode}"]`);
+    if(!audio) return;
+    audio.currentTime = 0;
+    audio.play();
+})
